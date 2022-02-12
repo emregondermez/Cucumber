c79201aec638052aa80d07f76685a0e5738a56f6
talfik2 <emregondermez@gmail.com>
2022-02-12 14:13:39 +0100

    12.02.2022

--- src/test/java/stepdefinitions/AmazonStepDefinitions.java
+++ src/test/java/stepdefinitions/AmazonStepDefinitions.java
@@ -91,21 +91,25 @@ public class AmazonStepDefinitions {
         hmcPage.mainPageLoginLinki.click();
     }
     @Then("kullanici adi olarak {string} girer")
-    public void kullanici_adi_olarak_girer(String string) {
+    public void kullanici_adi_olarak_girer(String username) {
         hmcPage = new HMCPage();
-        hmcPage.usernameTextBox.sendKeys("");
+        hmcPage.usernameTextBox.sendKeys(ConfigReader.getProperty(username));
+
+
     }
     @Then("password olarak {string} girer")
-    public void password_olarak_girer(String string) {
-
+    public void password_olarak_girer(String password) {
+        hmcPage = new HMCPage();
+        hmcPage.passwordTextBox.sendKeys(ConfigReader.getProperty(password));
     }
     @Then("Login butonuna basar")
     public void login_butonuna_basar() {
-
+        hmcPage.loginButonu.click();
     }
     @Then("basarili olarak giris yapildigini test eder")
     public void basarili_olarak_giris_yapildigini_test_eder() {
-
+    hmcPage=new HMCPage();
+    Assert.assertTrue(hmcPage.basariliGirisYaziElementi.isEnabled());
     }
 
 }
